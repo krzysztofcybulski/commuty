7cda635b96ee1df0e23de1f702a85c8eb8f8ad87
Krzysztof Dziadkowiec <krzysztof.dziadkowiec@allegro.pl>
2024-06-16 00:43:32 +0200

    timepicker

--- frontend/src/components/OnboardingView.tsx
+++ frontend/src/components/OnboardingView.tsx
@@ -1,31 +1,35 @@
-import {ContinueButton} from "./ContinueButton.tsx";
-import {TypographyH1} from "./TypographyH1.tsx";
-import React from "react";
-import {TypographyH2} from "./TypographyH2.tsx";
-import {BackButton} from "./BackButton.tsx";
+import { ContinueButton } from './ContinueButton.tsx';
+import { TypographyH1 } from './TypographyH1.tsx';
+import React from 'react';
+import { TypographyH2 } from './TypographyH2.tsx';
+import { BackButton } from './BackButton.tsx';
 
 export interface ViewConfig {
-    onContinueClick: () => void;
-    title?: string;
-    subTitle?: string;
-    buttonDisabled?: boolean;
+  onContinueClick: () => void;
+  title?: string;
+  subTitle?: string;
+  buttonDisabled?: boolean;
+  shouldShowBackButton?: boolean;
 }
 
 export interface OnboardingViewProps {
-    children: React.ReactNode;
-    config: ViewConfig
+  children: React.ReactNode;
+  config: ViewConfig;
 }
 
-export const OnboardingView = ({children, config}: OnboardingViewProps) => {
-    return <div className="w-full">
-        <div className="flex flex-col items-stretch justify-stretch w-full top-0 p-4">
-            <BackButton view="WELCOME"/>
-            <TypographyH1 text={config.title || ""} className="font-medium text-xl"/>
-            <TypographyH2 text={config.subTitle || ""} className="mt-2 font-light text-base"/>
-        </div>
-        {children}
-        <div className="fixed flex items-stretch justify-stretch w-full bottom-0">
-            { !config.buttonDisabled &&  <ContinueButton className="grow m-4" onClick={config.onContinueClick}/> }
-        </div>
+export const OnboardingView = ({ children, config }: OnboardingViewProps) => {
+  const { title, subTitle, buttonDisabled, shouldShowBackButton = true, onContinueClick } = config;
+  return (
+    <div className="w-full">
+      <div className="flex flex-col items-stretch justify-stretch w-full top-0 p-4">
+        {shouldShowBackButton && <BackButton view="WELCOME" />}
+        <TypographyH1 text={title || ''} className="font-medium text-xl" />
+        <TypographyH2 text={subTitle || ''} className="mt-2 font-light text-base" />
+      </div>
+      {children}
+      <div className="fixed flex items-stretch justify-stretch w-full bottom-0">
+        {!buttonDisabled && <ContinueButton className="grow m-4" onClick={onContinueClick} />}
+      </div>
     </div>
-}
+  );
+};
--- frontend/src/views/RootView.tsx
+++ frontend/src/views/RootView.tsx
@@ -1,90 +1,91 @@
-import {WelcomeView} from './WelcomeView.tsx';
-import {useEffect} from 'react';
-import {useUser} from '@clerk/clerk-react';
-import {useSaveUserData} from '../hooks/useSaveUserData.ts';
-import {useAppDispatch, useAppSelector} from '../store/store.ts';
-import {selectView, updateView} from '../store/appReducer.ts';
-import {SelectCommutingPreferencesView} from './SelectCommutingPreferencesView.tsx';
-import {WhenYouAreGoingView} from "./WhenYouAreGoingView.tsx";
-import {OnboardingView, ViewConfig} from "../components/OnboardingView.tsx";
-import {SetYourNameView} from './SetYourNameView.tsx';
-import {CreateAccountView} from "./CreateAccountView.tsx";
+import { WelcomeView } from './WelcomeView.tsx';
+import { useEffect } from 'react';
+import { useUser } from '@clerk/clerk-react';
+import { useSaveUserData } from '../hooks/useSaveUserData.ts';
+import { useAppDispatch, useAppSelector } from '../store/store.ts';
+import { selectView, updateView } from '../store/appReducer.ts';
+import { SelectCommutingPreferencesView } from './SelectCommutingPreferencesView.tsx';
+import { WhenYouAreGoingView } from './WhenYouAreGoingView.tsx';
+import { OnboardingView, ViewConfig } from '../components/OnboardingView.tsx';
+import { SetYourNameView } from './SetYourNameView.tsx';
+import { CreateAccountView } from './CreateAccountView.tsx';
 // import {useSelector} from "react-redux";
 
 export const RootView = () => {
-    const {user} = useUser();
-    const saveUserData = useSaveUserData();
-    const view = useAppSelector(selectView);
-    const dispatch = useAppDispatch();
-    // const payload = useSelector(selectPayload)
+  const { user } = useUser();
+  const saveUserData = useSaveUserData();
+  const view = useAppSelector(selectView);
+  const dispatch = useAppDispatch();
+  // const payload = useSelector(selectPayload)
 
-    useEffect(() => {
-        saveUserData(user);
-    }, []);
+  useEffect(() => {
+    saveUserData(user);
+  }, []);
 
-    const getView = () => {
-        switch (view) {
-            case 'WELCOME':
-                return <WelcomeView></WelcomeView>;
-            case 'SELECT_COMMUTING_PREFERENCES':
-                return <SelectCommutingPreferencesView></SelectCommutingPreferencesView>;
-            case 'WHEN_YOU_ARE_GOING':
-                return <WhenYouAreGoingView></WhenYouAreGoingView>;
-            case 'SET_YOUR_NAME':
-                return <SetYourNameView></SetYourNameView>;
-            case 'CREATE_ACCOUNT':
-                return <CreateAccountView></CreateAccountView>;
-        }
-    };
-
-    const configBasedOnView = (): ViewConfig | undefined => {
-        switch (view) {
-            case 'WELCOME':
-                return {
-                    onContinueClick: () => {
-                        dispatch(updateView('SELECT_COMMUTING_PREFERENCES'))
-                    },
-                    title: 'Hey! Tell us what are you looking for',
-                    subTitle: 'It’s fine to select both'
-                }
-            case 'SELECT_COMMUTING_PREFERENCES':
-                return {
-                    onContinueClick: () => {
-                        dispatch(updateView('WHEN_YOU_ARE_GOING'))
-                    },
-                    title: 'When are you going?',
-                    subTitle: 'It’s fine to select both'
-                }
-            case 'WHEN_YOU_ARE_GOING':
-                return {
-                    onContinueClick: () => {
-                        dispatch(updateView('SET_YOUR_NAME'))
-                    },
-                    title: 'Where are you commuting?',
-                    subTitle: 'It’s fine to select both'
-                }
-            case 'SET_YOUR_NAME':
-                return {
-                    onContinueClick: () => {
-                        dispatch(updateView('CREATE_ACCOUNT'))
-                    }
-                }
-            case 'CREATE_ACCOUNT':
-                return {
-                    onContinueClick: () => {
-                        console.log()
-                        // dispatch(updateView('HOME_PAGE'))
-                    },
-                    buttonDisabled: 'Create Account'
-                }
-            default:
-                return undefined;
-        }
+  const getView = () => {
+    switch (view) {
+      case 'WELCOME':
+        return <WelcomeView></WelcomeView>;
+      case 'SELECT_COMMUTING_PREFERENCES':
+        return <SelectCommutingPreferencesView></SelectCommutingPreferencesView>;
+      case 'WHEN_YOU_ARE_GOING':
+        return <WhenYouAreGoingView></WhenYouAreGoingView>;
+      case 'SET_YOUR_NAME':
+        return <SetYourNameView></SetYourNameView>;
+      case 'CREATE_ACCOUNT':
+        return <CreateAccountView></CreateAccountView>;
     }
+  };
 
-    return <div className="flex justify-stretch items-stretch w-full">
-        <OnboardingView config={configBasedOnView()!}>
-            {getView()}
-        </OnboardingView>
-    </div>;
+  const configBasedOnView = (): ViewConfig | undefined => {
+    switch (view) {
+      case 'WELCOME':
+        return {
+          onContinueClick: () => {
+            dispatch(updateView('SELECT_COMMUTING_PREFERENCES'));
+          },
+          title: 'Hey! Tell us what are you looking for',
+          subTitle: 'It’s fine to select both',
+          shouldShowBackButton: false,
+        };
+      case 'SELECT_COMMUTING_PREFERENCES':
+        return {
+          onContinueClick: () => {
+            dispatch(updateView('WHEN_YOU_ARE_GOING'));
+          },
+          title: 'When are you going?',
+          subTitle: 'It’s fine to select both',
+        };
+      case 'WHEN_YOU_ARE_GOING':
+        return {
+          onContinueClick: () => {
+            dispatch(updateView('SET_YOUR_NAME'));
+          },
+          title: 'Where are you commuting?',
+          subTitle: 'It’s fine to select both',
+        };
+      case 'SET_YOUR_NAME':
+        return {
+          onContinueClick: () => {
+            dispatch(updateView('CREATE_ACCOUNT'));
+          },
+        };
+      case 'CREATE_ACCOUNT':
+        return {
+          onContinueClick: () => {
+            console.log();
+            // dispatch(updateView('HOME_PAGE'))
+          },
+          buttonDisabled: 'Create Account',
+        };
+      default:
+        return undefined;
+    }
+  };
+
+  return (
+    <div className="flex justify-stretch items-stretch w-full">
+      <OnboardingView config={configBasedOnView()!}>{getView()}</OnboardingView>
+    </div>
+  );
 };
