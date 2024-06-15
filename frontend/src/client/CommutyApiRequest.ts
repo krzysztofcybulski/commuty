508307b22dd6ec6393bbf1536624c0a31232711a
Piotr Proszowski <p.proszowski@gmail.com>
2024-06-15 12:54:45 +0200

    Example commuty api request

---
+++ frontend/src/client/CommutyApiRequest.ts
@@ -0,0 +1,27 @@
+export interface CommutyApiRequest {
+    user: User;
+    ridePreferences: string[];
+    commutingRoutes: CommutingRoute[];
+}
+interface CommutingRoute {
+    addressFrom: PointOfInterest;
+    addressTo: PointOfInterest;
+    timePreferences: TimePreference[];
+}
+interface TimePreference {
+    day: string;
+    timeRange: TimeRange;
+}
+interface TimeRange {
+    departureTime: string; // localtime, example: 08:00
+    returnTime: string; // localtime
+}
+interface PointOfInterest {
+    longitude: string;
+    latitude: string;
+    levelOfDetail: number;
+}
+interface User {
+    name: string;
+    email: string;
+}
--- frontend/src/views/WelcomeView.tsx
+++ frontend/src/views/WelcomeView.tsx
@@ -3,13 +3,49 @@ import {TypographyH2} from "../components/TypographyH2.tsx";
 import {TypographyH3} from "../components/TypographyH3.tsx";
 import {useCommutyApi} from "../client/useCommutyApi.ts";
 import {useEffect} from "react";
+import {CommutyApiRequest} from "../client/CommutyApiRequest.ts";
+
+export const exampleRequest: CommutyApiRequest = {
+    "user": {
+        "name": "Piotrek",
+        "email": "p.proszowski@gmail.com"
+    },
+    "ridePreferences": [
+        "DRIVER",
+        "PASSENGER"
+    ],
+    "commutingRoutes": [
+        {
+            "addressFrom": {
+                "longitude": "52.2616832",
+                "latitude": "21.0501632",
+                "levelOfDetail": 13
+            },
+            "addressTo": {
+                "longitude": "52.2616832",
+                "latitude": "21.0501632",
+                "levelOfDetail": 13
+            },
+            "timePreferences": [
+                {
+                    "day": "MONDAY",
+                    "timeRange": {
+                        "departureTime": "8:00",
+                        "returnTime": "13:00"
+                    }
+                }
+            ]
+        }
+    ]
+}
+
 
 export const WelcomeView = () => {
 
     const {saveRoute} = useCommutyApi()
 
     useEffect(() => {
-        saveRoute()
+        saveRoute(exampleRequest)
     }, []);
 
     return <div className="container mx-auto p-8">
