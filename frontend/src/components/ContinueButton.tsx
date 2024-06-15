a840200ac9320e839481d28e1e77762f0c76f412
Piotr Proszowski <p.proszowski@gmail.com>
2024-06-15 19:17:01 +0200

    Update continue button

--- frontend/src/components/ContinueButton.tsx
+++ frontend/src/components/ContinueButton.tsx
@@ -4,13 +4,14 @@ interface ButtonProps {
   className?: string;
 }
 
-export const ContinueButton = ({ onClick, className, text = 'Continue ➡️' }: ButtonProps) => {
+export const ContinueButton = ({ onClick, className, text = 'Continue' }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
       className={`flex h-14 min-w-full rounded-lg bg-gray-800 text-white justify-center items-center text-xl ${className}`}
     >
-      <p>{text}</p>
+      <p className="pr-2">{text}</p>
+      <img className="w-4" src={"/arrow-right.png"}></img>
     </button>
   );
 };
