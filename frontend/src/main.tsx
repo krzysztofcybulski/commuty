456872cfe0cd3e3f7e692b1ee94e791cb31e82f3
Krzysztof Dziadkowiec <krzysztof.dziadkowiec@allegro.pl>
2024-06-15 12:12:59 +0200

    routing first setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import RootLayout from './layouts/rootLayout'
+import DashboardLayout from './layouts/dashboardLayout'
+import IndexPage from './routes'
+import SignInPage from './routes/signIn'
+import SignUpPage from './routes/signUp'
+import DashboardPage from './routes/dashboard'
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <IndexPage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        path: "dashboard",
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> },
+        ]
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
