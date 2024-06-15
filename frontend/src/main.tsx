2ec4afeba0e57a5a3bb6a1a5f51ed59cec3d66c5
Piotr Proszowski <p.proszowski@gmail.com>
2024-06-15 13:22:05 +0200

    Welcome view

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import RootLayout from './layouts/rootLayout';
 import DashboardLayout from './layouts/dashboardLayout';
 import SignInPage from './routes/signIn';
 import SignUpPage from './routes/signUp';
 import DashboardPage from './routes/dashboard';
 import About from './routes/about';
-import { Provider } from 'react-redux';
-import { store } from './store/store.ts';
-import IndexPage from './routes/index.tsx';
+import {Provider} from 'react-redux';
+import {store} from './store/store.ts';
+import {WelcomeView} from "./views/WelcomeView.tsx";
 
 const router = createBrowserRouter([
-  {
-    element: <RootLayout />,
-    children: [
-      { path: '/', element: <IndexPage /> },
-      { path: '/about/*', element: <About /> },
-      { path: '/sign-in/*', element: <SignInPage /> },
-      { path: '/sign-up/*', element: <SignUpPage /> },
-      {
-        element: <DashboardLayout />,
-        path: 'dashboard',
-        children: [{ path: '/dashboard', element: <DashboardPage /> }],
-      },
-    ],
-  },
+    {
+        element: <RootLayout/>,
+        children: [
+            {path: '/', element: <WelcomeView/>},
+            {path: '/about/*', element: <About/>},
+            {path: '/sign-in/*', element: <SignInPage/>},
+            {path: '/sign-up/*', element: <SignUpPage/>},
+            {
+                element: <DashboardLayout/>,
+                path: 'dashboard',
+                children: [{path: '/dashboard', element: <DashboardPage/>}],
+            },
+        ],
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>
+    </React.StrictMode>,
 );
