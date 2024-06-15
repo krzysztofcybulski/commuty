bf7e85864683d983d7bb3455a1f53997b5a4c595
bentozord <rafal.utnicki26@gmail.com>
2024-06-15 11:35:18 +0200

    backend teamplte init

---
+++ backend/src/main/java/io/commuty/CommutyApplication.java
@@ -0,0 +1,13 @@
+package io.commuty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommutyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommutyApplication.class, args);
+	}
+
+}
