5777a6e99b1eb2e734d97e7a0a8a09069c901020
Piotr Proszowski <p.proszowski@gmail.com>
2024-06-16 10:35:01 +0200

    SignOut button

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -1,10 +1,11 @@
 import {BottomMenu} from "../components/BottomMenu.tsx";
 import AnimateOnRender from "../components/AnimateOnRender.tsx";
+import {SignOutButton} from "@clerk/clerk-react";
 
 export const ProfileView = () => {
     return <div>
         <AnimateOnRender>
-            <div>Profile</div>
+            <SignOutButton></SignOutButton>
         </AnimateOnRender>
         <BottomMenu chosen={'PROFILE'}/>
     </div>
